b7a2c9d4fc7afc8e19722897440d402f69c226da
Carlos Cañada <ccanada05@gmail.com>
2025-10-27 21:46:47 +0100

    cambios

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,9 @@
-// types.ts (CORREGIDO)
 
-// Interfaz para la pregunta de la API (claves en INGLÉS)
+
+
 export interface PreguntaAPI {
     type: string;
-    difficulty: "easy" | "medium" | "hard"; // La API usa inglés
+    difficulty: "easy" | "medium" | "hard"; 
     category: string;
     question: string; 
     correct_answer: string; 
@@ -12,15 +12,14 @@ export interface PreguntaAPI {
 
 // Interfaz para la respuesta completa de la API
 export interface RespuestaAPI {
-    response_code: number; // <-- Clave en inglés
-    results: PreguntaAPI[]; // <-- Clave en inglés
+    response_code: number; 
+    results: PreguntaAPI[]; 
 }
 
-// Interfaz para nuestra pregunta "procesada"
-// (Esta la controlas tú, puede estar en español)
+
 export interface Pregunta extends PreguntaAPI {
-    respuestas: string[];
-    // Añadimos las claves en español para usarlas en el resto de la app
-    pregunta: string;
-    respuesta_correcta: string;
+    respuestas: string[];
+    
+    pregunta: string;
+    respuesta_correcta: string;
 }
